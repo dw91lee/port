ffc5f4489d50b7a20c4142d14dcc7d074122d350
dw91lee <96824185+dw91lee@users.noreply.github.com>
2022-01-18 12:08:58 +0900

    20220118

--- web_tj/js/common.js
+++ web_tj/js/common.js
@@ -7,7 +7,37 @@ $(function(){
         pauseOnHover : true,
       });
 
-	
+      $(function(){
+        $('#box .slick-slide').each(function(){ 
+            if($(window).width() < 769){ 
+              var img = $(this).find('img');
+              var img_pc = img.attr('src'); 
+              var img_mo = img_pc.replace('_pc','_mo')
+              img.attr('src',img_mo);
+           }else{
+              var img = $(this).find('img');
+              var img_mo = img.attr('src'); 
+              var img_pc = img_mo.replace('_mo','_pc')
+                img.attr('src',img_pc);
+              }
+         })  
+          $(window).resize(function(){
+            // 윈도우 창을 사용자가 움직이기 시작하면 이미지변수 다시 불러와야 함
+          $('#box .slick-slide').each(function(){ 
+            if($(window).width() < 769){ 
+              var img = $(this).find('img');
+              var img_pc = img.attr('src'); 
+              var img_mo = img_pc.replace('_pc','_mo')
+              img.attr('src',img_mo);
+           }else{
+              var img = $(this).find('img');
+              var img_mo = img.attr('src'); 
+              var img_pc = img_mo.replace('_mo','_pc')
+                img.attr('src',img_pc);
+              }
+          })  
+        })
+        })
         $('.top').click(function(){
           event.preventDefault();
       
